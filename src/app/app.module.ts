1c7e78f4604bcd5e7739b010e22f3aea2d78d915
SharedOps <manikanta.mundru@gmail.com>
2019-01-03 19:47:17 +0200

    Saaramsha project added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material/app-material.module';
+import { HomeComponent } from './controllers/home/home.component';
+import { AboutUsComponent } from './controllers/about-us/about-us.component';
+import { ContactUsComponent } from './controllers/contact-us/contact-us.component';
+import { ClientsComponent } from './controllers/clients/clients.component';
+import { ServicesComponent } from './controllers/services/services.component';
+import { PartnersComponent } from './controllers/partners/partners.component';
+import { LoginComponent } from './controllers/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    ClientsComponent,
+    ServicesComponent,
+    PartnersComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
